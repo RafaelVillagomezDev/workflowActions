db4b1b80195476bfa569e20cab48dd75ebfbe731
RafaelVillagomezDev <rafaelvillagomez.dev@gmail.com>
2022-04-20 14:09:16 +0200

    deploy 1

--- workflowactions/src/App.js
+++ workflowactions/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React a
+          Learn React funciona
         </a>
       </header>
     </div>
